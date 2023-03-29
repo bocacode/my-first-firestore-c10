f3a6d8f70ac8b55dee4baaa57fb4c27b6fc68121
Todd H. Albert <todd@bocacode.com>
2023-03-29 15:57:11 -0400

    reading and updating

--- index.js
+++ index.js
@@ -14,15 +14,48 @@ initializeApp({
 const db = getFirestore();
 
 // Add a product to our products collection
-const candy = {
-  name: "Skittles",
-  unitPrice: 3.99,
-  size: "16 oz",
-  color: "green",
-  inventory: 144,
-  productNumber: 7,
+const candy2 = {
+  name: "Twix",
+  unitPrice: 2.99,
+  size: "12 oz",
+  color: "gold",
+  inventory: 288,
+  productNumber: 2,
 }
 
-db.collection('products').add(candy)
-  .then(doc => console.log("added doc: " + doc.id))
-  .catch(err => console.log(err))
+// How to add a document to Firestore:
+
+// db.collection('products').add(candy2) // While we are waiting for the promise...
+//   .then((doc) => {
+//     console.log("added doc: " + doc.id)
+//     // I can be sure inside .then() that the first process was completed successfully
+//   })
+//   .catch(err => console.log(err))
+
+// db.collection('products').doc('Aw6bH9bExqSbe7Kr8LJ4').delete()
+
+// How to update a document in Firestore:
+db.collection('products').doc('Aw6bH9bExqSbe7Kr8LJ4').update({
+  inventory: 555,
+  customerFavorite: true
+})
+
+
+// How to read a document from Firestore:
+
+db.collection('products').doc('Aw6bH9bExqSbe7Kr8LJ4').get()
+  .then(doc => {
+    console.log(doc.data());
+  })
+  .catch(err => console.log(err));
+
+
+  // How to get a whole collection:
+
+db.collection('products').get()
+  .then(collection => {
+    const productList = collection.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    console.table(productList);
+  })
+  .catch(console.log);
+
